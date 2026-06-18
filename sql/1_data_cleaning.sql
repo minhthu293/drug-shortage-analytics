00b78256b6f64f549ba77264f1ef4b7d6cfb6654
Lê Thị Minh Thư <lethiminhthu293@gmail.com>
2026-06-18 16:43:33 +0700

    Update 1_data_cleaning.sql

--- sql/1_data_cleaning.sql
+++ sql/1_data_cleaning.sql
@@ -20,3 +20,13 @@ USING TO_DATE(initial_posting_date, 'MM/DD/YYYY');
 ALTER TABLE drug_shortages_clean
 ALTER COLUMN update_date TYPE DATE
 USING TO_DATE(update_date, 'MM/DD/YYYY');
+
+
+-- Bước 4: Chuẩn hóa cột discontinued_date từ TEXT sang DATE
+-- (Định dạng gốc: MM/DD/YYYY, chứa 1,176 dòng trống cần chuyển thành NULL)
+ALTER TABLE drug_shortages_clean
+ALTER COLUMN discontinued_date TYPE DATE
+USING CASE 
+    WHEN discontinued_date = '' THEN NULL
+    ELSE TO_DATE(discontinued_date, 'MM/DD/YYYY') 
+END;
